21b4b112565967d3a2f341df7568d144f19bc914
Akshat-Datt <dattakshat31@gmail.com>
2026-02-07 13:03:46 +0530

    setting up game fragment and game layout

--- app/src/main/java/com/unit/trivia/GameFragment.kt
+++ app/src/main/java/com/unit/trivia/GameFragment.kt
@@ -5,15 +5,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.currentRecomposeScope
+import androidx.databinding.DataBindingUtil
+import com.unit.trivia.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    data class Question(
+        val text: String,
+        val answers: List<String>)
+
+    private val questions: MutableList<Question> = mutableListOf(
+        Question(text = "What is Android Jetpack?",
+            answers = listOf("all of these", "tools", "documentation", "libraries")),
+        Question(text = "Base class for Layout?",
+            answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")),
+        Question(text = "Layout for complex Screens?",
+            answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")),
+        Question(text = "Pushing structured data into a Layout?",
+            answers = listOf("Data Binding", "Data Pushing", "Set Text", "OnClick")),
+        Question(text = "Inflate layout in fragments?",
+            answers = listOf("onCreateView", "onViewCreated", "onCreateLayout", "onInflateLayout")),
+        Question(text = "Build system for Android?",
+            answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")),
+        Question(text = "Android vector format?",
+            answers = listOf("VectorDrawable", "AndroidVectorDrawable", "DrawableVector", "AndroidVector")),
+        Question(text = "Android Navigation Component?",
+            answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")),
+        Question(text = "Registers app with launcher?",
+            answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")),
+        Question(text = "Mark a layout for Data Binding?",
+            answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))
+    )
+    lateinit var currentQuestion: Question
+    lateinit var answers: MutableList<String>
+    private var questionIndex = 0
+    private val numQuestions = Math.min((questions.size + 1) / 2, 3)
+    private lateinit var binding: FragmentGameBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+        randomizeQuestions()
+
+        binding.game = this
+
+        return binding.root;
+    }
+
+    private fun randomizeQuestions(){
+        questions.shuffle()
+        questionIndex = 0
+
+        setQuestion()
+    }
+
+    private fun setQuestion(){
+        currentQuestion = questions[questionIndex]
+        answers = currentQuestion.answers.toMutableList()
+        answers.shuffle()
+
     }
 
 }
