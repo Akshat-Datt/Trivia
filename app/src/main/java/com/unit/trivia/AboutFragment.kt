97633489c1ad53f9736affda0585558b86e40758
Akshat-Datt <dattakshat31@gmail.com>
2026-02-12 16:48:58 +0530

    adding an tem in action bar for redirection to about fragment

---
+++ app/src/main/java/com/unit/trivia/AboutFragment.kt
@@ -0,0 +1,22 @@
+package com.unit.trivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.unit.trivia.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+    private lateinit var binding: FragmentAboutBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false)
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/unit/trivia/TitleFragment.kt
+++ app/src/main/java/com/unit/trivia/TitleFragment.kt
@@ -3,10 +3,14 @@ package com.unit.trivia
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.unit.trivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -22,8 +26,18 @@ class TitleFragment : Fragment() {
         binding.button.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
-
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+    inflater?.inflate(R.menu.overflow_menu, menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 
 }
